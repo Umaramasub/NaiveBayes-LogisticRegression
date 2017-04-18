54195670f006fe32a3c6e6b105c443b4d84a17a7
umaramasub <uramasubramanian@usfca.edu>
2017-04-18 16:14:29 -0700

    Add function to get the list of all files in the directory

--- src/lab5.py
+++ src/lab5.py
@@ -1,5 +1,5 @@
 import pickle
-
+import os
 def read_pickle(filename):
     """Reads a pickle file with name and path of filename, returns the object
     :param filename: Name of the pickle file
@@ -10,3 +10,19 @@ def read_pickle(filename):
     return pickle.load(file)
 
 
+def get_files(dir_path):
+    """Given an absolute path to a particular directory, return a list of
+    absolute paths and filenames for files within the directory.  The intent is
+    that the return value of this function can be used with read_pickle()
+    without additional munging of paths/filenames.
+
+    :param dir_path: directory path containing files of interest
+    :return: list of filenames within directory
+    """
+    result = []
+    # Creates a list with filenames
+    list_filenames = os.listdir(os.path.normpath(dir_path))
+    # Loop over each filename and concatenate it to the absolute path
+    for file_name in list_filenames:
+        result.append(os.path.join(os.path.normpath(dir_path), file_name))
+    return result
